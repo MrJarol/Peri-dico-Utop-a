b016842459ba987b21f857b39b5753fd66989401
MrJarol <jarolsuarez8@gmail.com>
2026-07-22 19:53:29 -0500

    Subir proyecto inicial

---
+++ firebase.js
@@ -0,0 +1,38 @@
+<<<<<<< HEAD
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBu8AmOw3CdCp2huh2vtsfvgqmK3AQUYHM",
+  authDomain: "periodico-utopia.firebaseapp.com",
+  projectId: "periodico-utopia",
+  storageBucket: "periodico-utopia.firebasestorage.app",
+  messagingSenderId: "636872397282",
+  appId: "1:636872397282:web:7e5a40e0bac022af7429bb"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+=======
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBu8AmOw3CdCp2huh2vtsfvgqmK3AQUYHM",
+  authDomain: "periodico-utopia.firebaseapp.com",
+  projectId: "periodico-utopia",
+  storageBucket: "periodico-utopia.firebasestorage.app",
+  messagingSenderId: "636872397282",
+  appId: "1:636872397282:web:7e5a40e0bac022af7429bb"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+>>>>>>> a688dc27766ddea1cd42242b100221ae28c873c9
+export { db };
