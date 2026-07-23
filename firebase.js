87417da6a3997155c16656e8b305d5a0be10ceb3
MrJarol <jarolsuarez8@gmail.com>
2026-07-22 20:55:04 -0500

    Corregir firebase duplicado

--- firebase.js
+++ firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
-
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -12,24 +11,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
-const db = getFirestore(app);
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
-
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBu8AmOw3CdCp2huh2vtsfvgqmK3AQUYHM",
-  authDomain: "periodico-utopia.firebaseapp.com",
-  projectId: "periodico-utopia",
-  storageBucket: "periodico-utopia.firebasestorage.app",
-  messagingSenderId: "636872397282",
-  appId: "1:636872397282:web:7e5a40e0bac022af7429bb"
-};
-
-const app = initializeApp(firebaseConfig);
-
-const db = getFirestore(app);
+const db = getFirestore(app); 
 
 export { db };
